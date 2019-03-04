464aa97aec0e6364648b48c976619acc4e6c8c85
Aaron Lathrop <Aaron.S.Lathrop@gmail.com>
2019-03-04 11:12:59 -0700

    add basic header with loop through skills

--- index.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-    $('.hamburger__container').click(() => {
-        $('.hamburger__container').toggleClass('change');
-        $('.nav__list').toggleClass('dropdown');
-    });
-
-});
---
+++ js/index.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+    $('.hamburger__container').click(() => {
+        $('.hamburger__container').toggleClass('change');
+        $('.nav__list').toggleClass('dropdown');
+    });
+
+    const skills = ["Javascript", "NodeJS", "React", "Redux", "MongoDB", "PostgreSQL"];
+    const numberOfSkills = skills.length;
+    let index = 0;
+
+    setInterval(function() {
+        $('#developer-skills').text(skills[index]);
+        index = (index + 1) % numberOfSkills;
+    }, 3000)
+
+});
